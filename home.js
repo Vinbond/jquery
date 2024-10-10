4fead345dcdc7df25794a7ef86c0817649d7225d
vinbond <vinayakkundangar77@gmail.com>
2024-10-10 22:14:42 +0530

    _

---
+++ home.js
@@ -0,0 +1,24 @@
+
+// console.log($)
+// function _hh(){
+//     //
+//      let _dsd=parseInt(prompt("Enter your first number"))
+//      let _ewew=parseInt(prompt("Enter ur second number"))
+//      let sum=_dsd+_ewew
+//      document.writeln(`${_dsd} + ${_ewew}=${sum}`)
+//     // let ee=document.getElementById(enter)
+//     // document.writeln(ee)
+
+// }
+${document}.ready(function(){
+    ${"#divi"}
+
+
+
+})
+
+
+
+
+
+
